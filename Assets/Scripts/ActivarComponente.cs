d43835c6d958312a6fc964179ad2743f1dd9a665
seriousname-90 <carvamen09@gmail.com>
2026-05-03 23:22:17 -0600

    feat: assemble scene locution with cuts

--- Assets/Scripts/ActivarComponente.cs
+++ Assets/Scripts/ActivarComponente.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class ActivarComponente : MonoBehaviour
 {
     public GameObject componente; // Arrastra cualquier componente (Building Block)
+    public AudioSource audioSource; // Arrastra el AudioSource que deseas reproducir  
+ 
+    public bool activarAlIniciar; // Si quieres que se active al iniciar
+
+    void Start()
+    {
+        if (activarAlIniciar)
+        {
+            ActivarDespuesDeSegundos();
+        }
+    }
 
     public void Activar()
     {
@@ -27,5 +38,20 @@ public class ActivarComponente : MonoBehaviour
         yield return new WaitForSeconds(0.01f);
         if (componente != null)
             componente.SetActive(false);
+    }
+
+    public void ActivarDespuesDeSegundos()
+    {
+        StartCoroutine(ActivarDelay());
+    }
+
+    IEnumerator ActivarDelay()
+    {
+        yield return new WaitForSeconds(2.5f);
+        if (audioSource != null)
+            audioSource.PlayOneShot(audioSource.clip);
+
+        yield return new WaitForSeconds(audioSource.clip.length);
+        Activar();
     }
 }
