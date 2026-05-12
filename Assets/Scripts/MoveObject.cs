a1dbb872a1055dcb3a73ba9d9b7c803018bdf5db
seriousname-90 <carvamen09@gmail.com>
2026-05-12 13:20:30 -0600

    update: configured locution

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -1,21 +1,32 @@
 using UnityEngine;
-using UnityEngine.Events; // Necesario para usar eventos
+using UnityEngine.Events;
 
 public class MoveObject : MonoBehaviour
 {
     public Vector3 posicionDestino;
+    public Vector3 rotacionDestino;
     public float velocidad = 2f;
     
     [Header("Eventos de Finalización")]
     public UnityEvent alTerminarMovimiento;
     
     private Vector3 posicionInicial;
+    private Quaternion rotacionInicial;
     private float tiempoRecorrido = 0f;
     private bool moviendo = false;
     
     public void IniciarMovimiento()
     {
         posicionInicial = transform.position;
+        rotacionInicial = transform.rotation;
+        tiempoRecorrido = 0f;
+        moviendo = true;
+    }
+    
+    public void IniciarMovimientoR()
+    {
+        posicionInicial = transform.position;
+        rotacionInicial = transform.rotation;
         tiempoRecorrido = 0f;
         moviendo = true;
     }
@@ -25,11 +36,18 @@ public class MoveObject : MonoBehaviour
         if (moviendo)
         {
             tiempoRecorrido += Time.deltaTime * velocidad;
+            
+            // Movimiento de posición
             transform.position = Vector3.Lerp(posicionInicial, posicionDestino, tiempoRecorrido);
+            
+            // Movimiento de rotación
+            Quaternion rotacionDestinoQuaternion = Quaternion.Euler(rotacionDestino);
+            transform.rotation = Quaternion.Lerp(rotacionInicial, rotacionDestinoQuaternion, tiempoRecorrido);
             
             if (tiempoRecorrido >= 1f)
             {
                 transform.position = posicionDestino;
+                transform.rotation = Quaternion.Euler(rotacionDestino);
                 moviendo = false;
                 
                 if (alTerminarMovimiento != null)
